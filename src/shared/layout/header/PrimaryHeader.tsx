b7c2887cfedb9f91d8b75233302bafb750f02f01
sonhoai27 <sonhoai272@gmail.com>
2019-01-09 18:09:48 +0700

    fix bug submenu

--- src/shared/layout/header/PrimaryHeader.tsx
+++ src/shared/layout/header/PrimaryHeader.tsx
@@ -1,32 +1,38 @@
 import * as React from 'react';
 
 import './styles/PrimaryHeader.scss'
+import SubMenu from './components/SubMenu';
 
 const PrimaryHeader = () => (
   <div className="col-12 primary-header">
     <div className="container">
       <div className="row">
-        <div className="col-sm-2">
+        <div className="primary-header__logo">
           <img
             src="http://en.klavuu.com/img/logo/logo.png"
-            className="img-fluid logo"/>
+            className="img-fluid logo" />
         </div>
-        <div className="col-sm-10">
-          <ul>
-            <li>
-              <a href="#">Shop By</a>
-            </li>
-            <li>
-              <a href="#">New arrivals</a>
-            </li>
-            <li>
-              <a href="#">Brands</a>
-            </li>
-            <li>
-              <a href="#">About us</a>
-            </li>
-          </ul>
-        </div>
+        <ul>
+          <li>
+            <a href="#">Shop By</a>
+            <SubMenu className="primary-menu__submenu" />
+          </li>
+          <li>
+            <a href="#">New arrivals</a>
+          </li>
+          <li>
+            <a href="#">Brands</a>
+          </li>
+          <li>
+            <a href="#">Sales</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+          <li>
+            <a href="#">Contact us</a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
